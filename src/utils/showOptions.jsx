0b336ac8a53336ecab70fe8dd125767326c249e5
Lars Petter Bø <115019135+LarsPetter98@users.noreply.github.com>
2024-02-11 22:44:34 +0100

    Delete src directory

--- src/utils/showOptions.jsx
+++
@@ -1,5 +0,0 @@
-export default function showOptions (event) {
-    const todoList = event.currentTarget.parentNode;
-    const taskOptions = todoList.children[todoList.children.length - 1];
-    taskOptions.classList.toggle("hide");
-};
