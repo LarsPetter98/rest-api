df6d289bd8efb4072d3cfa59abd8575347640c35
Lars Petter Bø <115019135+LarsPetter98@users.noreply.github.com>
2024-02-13 00:02:47 +0100

    Update routes.mjs

--- backend/routes.mjs
+++ backend/routes.mjs
@@ -10,14 +10,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 //Handling get requests
-router.get("/", (request, response, next) => {
+router.get("/"), (request, response, next) => {
     response.sendFile(path.join(__dirname, "../frontend/public"), error => {
         if (error) response.status(404).json({message: "Resource was not found"});
     });
-});
+};
 
 //Handling post requests
-router.post("/", async (request, response, next) => {
+router.post("/api/notes", async (request, response, next) => {
     try{
         let objectId = new mongoose.Types.ObjectId().toString();
         objectId = objectId.match(/[a-fA-F0-9]{24}/)[0];
@@ -34,7 +34,7 @@ router.post("/", async (request, response, next) => {
 });
 
 //Handling delete requests
-router.delete("/", async (request, response, next) => {
+router.delete("/api/notes", async (request, response, next) => {
     try {
         const content = request.body.content;
         await Note.deleteOne({content: content});
@@ -42,6 +42,22 @@ router.delete("/", async (request, response, next) => {
     }
     catch (error) {
         response.status(500).json({ error: error.message });
+    }
+});
+
+//Handling patch requests
+router.patch("/api/notes", async (request, response, next) => {
+    try {
+        const oldContent = request.body.oldContent; // Original content
+        const newContent = request.body.newContent; // Array of update operations
+
+        console.log(oldContent);
+        console.log(newContent);
+        await Note.updateOne({content: oldContent}, { $set: {content: newContent}});
+        response.status(200).json({message: "Note edited"});
+
+    } catch (error) {
+        response.status(500).json({error: error});
     }
 });
 
